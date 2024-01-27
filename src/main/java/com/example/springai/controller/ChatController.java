667ad6246d4312407109d87275414667025e30c4
Ravi Kumar Gupta <babaravibbb@gmail.com>
2024-01-27 21:07:02 +0530

    Spring AI

---
+++ src/main/java/com/example/springai/controller/ChatController.java
@@ -0,0 +1,33 @@
+package com.example.springai.controller;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+
+    @Autowired
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+
+    }
+
+    @GetMapping("/open-ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatClient.call(message));
+    }
+
+   /* @GetMapping("/open-ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return streamingChatClient.generate(prompt);
+    }*/
+}
